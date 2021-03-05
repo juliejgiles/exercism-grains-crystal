fa401f1b1c63e5b8a0b623c57ad9338575c20c45
Caspian Baska <caspianbaska@gmail.com>
2021-03-05 15:21:42 +1100

    perf: pre-compute values

--- src/grains.cr
+++ src/grains.cr
@@ -1,11 +1,15 @@
 class Grains
-  def self.square(grain : Number) : Int128
-    raise ArgumentError.new("grain must be between 1 and 64 inclusive") unless grain.in?(1..64)
-    2_i128**(grain - 1)
-  end
+  # We can totally precompute all the values
+  class_getter lookup : Array(Int128) { (0...64).map { |i| 2_i128**i } }
 
   # This is called a lazy getter.
+  #
   # The getter/property is nil until called and then returns the result
   # of the first call to the block for all future calls.
-  class_getter total : Int128 { (0...64).sum { |number| 2_i128**number } }
+  class_getter total : Int128 { lookup.sum }
+
+  def self.square(grain : Number) : Int128
+    raise ArgumentError.new("grain must be between 1 and 64 inclusive") unless grain.in?(1..64)
+    lookup[grain - 1]
+  end
 end
